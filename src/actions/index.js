94aec8c6aced1fa65b0241f1402d08bc562361fb
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-23 12:58:21 -0700

    Displaying list data in progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
 import types from './types';
+
+const API_KEY = '?key=c618_demouser';
---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render(){
+        return (
+            <ul className="collection">
+                <li className="collection-item">This will be the todo list</li>
+            </ul>
+        )
+    }
+}
+
+export default List;
