96aca36082eb1299e6d9c6f230bb580959f9fa92
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-29 16:42:57 -0700

    Item view with delete complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,23 @@ export function getAllListData(){
     };
 }
 
+export const getSingleItem = id => async dispatch => {
+
+    try {
+        const resp = await axios.get(`${BASE_URL}/todos/${id + API_KEY}`);
+
+        dispatch({
+            type: types.GET_SINGLE_ITEM,
+            payload: resp
+        });
+    } catch(err){
+        dispatch({
+            type: types.LIST_ERROR,
+            error: 'No item found'
+        });
+    }
+}
+
 export function addItem(item){
     const resp = axios.post(`${BASE_URL}/todos${API_KEY}`, item);
 
@@ -21,3 +38,31 @@ export function addItem(item){
         payload: resp
     };
 }
+
+export function deleteItem(id){
+    return async function (dispatch){
+        try {
+            const resp = await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
+
+            console.log('DELETE WORKED!');
+            dispatch({
+                type: types.DELETE_ITEM,
+                payload: resp
+            });
+        } catch(err){
+            console.log('DELETE FAILED!');
+            dispatch({
+                type: types.LIST_ERROR,
+                error: 'Failed to delete item'
+            });
+        }
+    }
+}
+
+export const resetSingle = () => ({type: types.RESET_SINGLE_VIEW});
+
+// export function resetSingle(){
+//     return {
+//         type: types.RESET_SINGLE_VIEW
+//     };
+// }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,8 @@
 export default {
     ADD_ITEM: 'add_item',
-    GET_ALL_LIST_DATA: 'get_all_list_data'
+    DELETE_ITEM: 'delete_item',
+    GET_ALL_LIST_DATA: 'get_all_list_data',
+    GET_SINGLE_ITEM: 'get_single_item',
+    LIST_ERROR: 'list_error',
+    RESET_SINGLE_VIEW: 'reset_single_view'
 };
